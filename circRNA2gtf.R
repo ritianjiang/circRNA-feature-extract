920c0ad0b38b8047d86ff349c30f6f7ef3ee09a2
Haotian Wang <ritianjiang@gmx.com>
2018-04-04 16:07:23 +0800

    Update circRNA2gtf.R

--- circRNA2gtf.R
+++ circRNA2gtf.R
@@ -22,33 +22,6 @@ for (i in 1:length(filelist)){
 }
 
 #extract the fasta seq by gffread
-gtflist<-list.files(pattern="*gff")
-for (i in 1:length(gtflist)){
-  command<-paste0("gffread ",gtflist[i],
-                 " -g ",
-                 "~/KIZ/shuju_from_web/RefNome/Human/fa/GRCh37.p13.genome.fa -w ",
-                 gtflist[i],".fasta") #generate the command used in shell
-  system(command,intern = T)
-  
-}
-
-#####heatmap 
-##only for test witll remove later
-plot_heat<-function(a,b){
-  commonTheme = list(labs(color="Density",fill="Density",
-                          x="daiding",
-                          y="daiding2"),
-                     theme_bw(),
-                     theme(legend.position=c(0,1),
-                           legend.justification=c(0,1)))
-  testh<-data.frame(a,b)
-  ggplot(data = testh,aes(a,b))+
-    stat_density2d(aes(fill=..level..,alpha=..level..),geom='polygon',colour='black') + 
-    scale_fill_continuous(low="green",high="red") +
-    geom_smooth(method=lm,linetype=2,colour="red",se=F) + 
-    guides(alpha="none") +
-    geom_point() + commonTheme
-}
-
+source("./heatplot.R")
 
 ######
